d7e5d447d2bc94218de218f12f0abe6c2e32dcd5
Reishi Mitani <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2021-01-25 22:41:45 +0900

    added configuration in photolist

--- src/component/PhotosList.tsx
+++ src/component/PhotosList.tsx
@@ -1,18 +1,28 @@
 import { memo } from 'react'
 import { S3Image } from 'aws-amplify-react'
 import { Divider } from 'semantic-ui-react'
+import Amplify from 'aws-amplify'
 
 type props = {
     photos: any
 }
 
+Amplify.configure({
+    aws_project_region: process.env.project_region,
+    aws_cognito_identity_pool_id: process.env.cognito_identity_pool_id,
+    aws_cognito_region: process.env.cognito_region,
+    aws_user_pools_id: process.env.user_pools_id,
+    aws_user_pools_web_client_id: process.env.user_pools_web_client_id,
+    oauth: {},
+    aws_appsync_graphqlEndpoint: process.env.appsync_graphqlEndpoint,
+    aws_appsync_region: process.env.appsync_region,
+    aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
+    aws_user_files_s3_bucket: process.env.user_files_s3_bucket,
+    aws_user_files_s3_bucket_region: process.env.user_files_s3_bucket_region,
+})
+
 const PhotosList = memo(({ photos }: props) => {
-    console.log('photos')
-    console.log(photos)
     const PhotoItems = (props) => {
-        console.log('props')
-        console.log(props)
-        console.log(props.photos)
         return props.photos.map((photo) => (
             <S3Image
                 key={photo.thumbnail.key}
